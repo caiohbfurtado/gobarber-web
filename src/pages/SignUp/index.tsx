bf96269bd100c9c9b0df3619f2c091dfdda080ec
Caio Barutti <caiohbfurtado@gmail.com>
2020-12-11 20:54:01 -0300

    add schema validation to the inputs

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import * as S from './styles';
 
@@ -10,9 +14,25 @@ import Button from '../../components/Button';
 import logo from '../../assets/logo.svg';
 
 const SignUp: React.FC = () => {
-  function handleSumbit(data: any): void {
-    console.log(data);
-  }
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSumbit = useCallback(async (data: any) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        name: Yup.string().required('O nome é obrigatório.'),
+        email: Yup.string()
+          .required('O e-mail é obrigatório.')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 digitos.'),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+    } catch (error) {
+      const errors = getValidationErrors(error);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
 
   return (
     <S.Container>
@@ -20,7 +40,7 @@ const SignUp: React.FC = () => {
       <S.Content>
         <img src={logo} alt="Go Barber" />
 
-        <Form onSubmit={handleSumbit}>
+        <Form ref={formRef} onSubmit={handleSumbit}>
           <h1>Faça seu cadastro</h1>
           <Input type="text" name="name" placeholder="Nome" icon={FiUser} />
           <Input type="text" name="email" placeholder="E-mail" icon={FiMail} />
